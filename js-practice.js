784e54c4805ff55ad5da52a1ac82687d2a9745ca
mike840 <mbj840@me.com>
2016-03-31 15:17:37 +0100

    Got external js working

--- js-practice.js
+++ js-practice.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-    makeLeftButtons();
-});
+//function makeLeftButtons() {
+//    console.log('ready');
+//}
 
 function makeLeftButtons() {
     var buttonTitleList = ['Div1', 'Div2', 'Div3', 'This is a very long title', 'Div5', 'Div6', 'Div7'];
 
-    window.onload = adjustHeight;
-    window.onresize = adjustHeight;
 
     for (var i = 0; i < buttonTitleList.length; i++) {
 
@@ -21,32 +19,37 @@ function makeLeftButtons() {
         div.innerHTML = '<p>' + buttonTitleList[i] + '</p>';
         document.getElementById('buttonDiv' + i).appendChild(div);
     }
+    adjustHeight();
+}
 
-    function adjustHeight() {
-        var buttonContainer = document.getElementById('buttonContainerDiv');
-        var numKids = buttonContainer.childNodes.length;
-        for (var i = 0; i < numKids; i++) {
-            var toChange = document.getElementById('buttonDiv' + i);
-            var newHeight = buttonContainer.clientHeight / numKids;
-            toChange.style.height = newHeight + 'px';
-            toChange.firstChild.style.marginTop = (toChange.clientHeight / 2) - (toChange.firstChild.clientHeight / 2) + 'px';
 
-        }
-    }
+ function adjustHeight() {
+    var buttonContainer = document.getElementById('buttonContainerDiv');
+    var numKids = buttonContainer.childNodes.length;
+    for (var i = 0; i < numKids; i++) {
+        var toChange = document.getElementById('buttonDiv' + i);
+        var newHeight = buttonContainer.clientHeight / numKids;
+        toChange.style.height = newHeight + 'px';
+        toChange.firstChild.style.marginTop = (toChange.clientHeight / 2) - (toChange.firstChild.clientHeight / 2) + 'px';
 
-    function clickedFunc() {
-
-        for (var i = 0; i < this.parentNode.childNodes.length; i++) {
-            this.parentNode.childNodes[i].setAttribute('class', 'buttonDiv');
-        }
-        this.setAttribute('class', 'buttonDiv clickedBtn');
-    }
-
-    function overFunc() {
-        this.setAttribute('class', 'buttonDiv mouseOver');
-    }
-
-    function outFunc() {
-        this.setAttribute('class', 'buttonDiv');
     }
 }
+
+function clickedFunc() {
+
+    for (var i = 0; i < this.parentNode.childNodes.length; i++) {
+        this.parentNode.childNodes[i].setAttribute('class', 'buttonDiv');
+    }
+    this.setAttribute('class', 'buttonDiv clickedBtn');
+}
+
+function overFunc() {
+    this.setAttribute('class', 'buttonDiv mouseOver');
+}
+
+function outFunc() {
+    this.setAttribute('class', 'buttonDiv');
+}
+
+window.onresize = adjustHeight;
+
