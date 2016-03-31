0dce9864f77b15035db8a157f4e9a2108e321d9b
mike840 <mbj840@me.com>
2016-03-31 23:45:25 +0100

    Changing to jquery

--- js-practice.js
+++ js-practice.js
@@ -3,21 +3,23 @@
 //}
 
 function makeLeftButtons() {
-    var buttonTitleList = ['Text', 'Gallery', 'Favorites', 'Image', 'Share', 'Settings'];
+    var buttonTitleList = ['Text', 'Gallery', 'Favorites', 'Image', 'Share', 'Settings', 'Other'];
 
 
     for (var i = 0; i < buttonTitleList.length; i++) {
 
         var div = document.createElement('div');
-        div.setAttribute('class', 'buttonDiv');
-        div.setAttribute('id', 'buttonDiv' + i);
-        div.addEventListener('click', clickedFunc);
-        document.getElementById('buttonContainerDiv').appendChild(div);
+        var $div = $(div);
+        $div.attr('class', 'buttonDiv');
+        $div.attr('id', 'buttonDiv' + i);
+        $div.on('click', clickedFunc);
+        $('#buttonContainerDiv').append(div);
 
         var div = document.createElement('div');
-        div.setAttribute('class', 'btnTitleDiv');
-        div.innerHTML = buttonTitleList[i];
-        document.getElementById('buttonDiv' + i).appendChild(div);
+        var $div = $(div);
+        $div.attr('class', 'btnTitleDiv');
+        $div.html(buttonTitleList[i]);
+        $('#buttonDiv' + i).append(div);
     }
     adjustHeight();
 }
@@ -29,13 +31,12 @@ function makeLeftButtons() {
     var newHeight = Math.floor(buttonContainer.clientHeight / numKids);
     var heightCount = 0;
     var lastButtonHeight = 0;
-    for (var i = 0; i < numKids; i++) {
+    for (var i = 0; i < numKids-1; i++) {
         var toChange = document.getElementById('buttonDiv' + i);
         toChange.style.height = newHeight + 'px';
         heightCount += newHeight;
         console.log(heightCount);
     }
-    heightCount -= newHeight;
     lastButtonHeight = buttonContainer.clientHeight - heightCount;
     buttonContainer.lastChild.style.height = lastButtonHeight;
 }
@@ -45,7 +46,7 @@ function clickedFunc() {
     for (var i = 0; i < this.parentNode.childNodes.length; i++) {
         this.parentNode.childNodes[i].setAttribute('class', 'buttonDiv');
     }
-    this.setAttribute('class', 'buttonDiv clickedBtn');
+    this.setAttribute('class', 'buttonDiv clicked');
 }
 
 function overFunc() {
