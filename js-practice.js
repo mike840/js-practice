23d5d031b925e5949e88ef3408391b64b8548d66
mike840 <mbj840@me.com>
2016-04-01 18:21:38 +0100

    Made button function non-specific. And now mostly jquery.

--- js-practice.js
+++ js-practice.js
@@ -1,42 +1,45 @@
-function makeLeftButtons() {
-    var buttonTitleList = ['Text', 'Gallery', 'Favorites', 'Image', 'Share', 'Settings', 'Other'];
+var leftColumnTitles = ['Text', 'Gallery', 'Favorites', 'Image', 'Share', 'Settings', 'Other'];
+
+function makeButtonColumn(buttonTitles, buttonTitleClass, buttonClass, buttonID, buttonParent) {
 
 
-    for (var i = 0; i < buttonTitleList.length; i++) {
-
+    for (var i = 0; i < buttonTitles.length; i++) {
+        console.log(buttonTitles.length);
         var div = document.createElement('div');
         var $div = $(div);
-        $div.attr('class', 'buttonDiv');
-        $div.attr('id', 'buttonDiv' + i);
+        $div.addClass(buttonClass);
+        $div.attr('id', buttonID + i);
+
         $div.on('click', function(){
             clickedFunc($(this));
         });
-        $('#buttonContainerDiv').append(div);
+        $(buttonParent).append(div);
 
         var div = document.createElement('div');
         var $div = $(div);
-        $div.attr('class', 'btnTitleDiv');
-        $div.html(buttonTitleList[i]);
-        $('#buttonDiv' + i).append(div);
+        $div.addClass(buttonTitleClass);
+        $div.html(buttonTitles[i]);
+        console.log(buttonTitles[i]);
+        $('#' + buttonID + i).append(div);
     }
     adjustHeight();
 }
 
 
  function adjustHeight() {
-    var buttonContainer = document.getElementById('buttonContainerDiv');
-    var numKids = buttonContainer.childNodes.length;
-    var newHeight = Math.floor(buttonContainer.clientHeight / numKids);
+    var $buttonContainer = $('#buttonContainerDiv');
+    var numKids = $buttonContainer.children().length;
+    var newHeight = Math.floor($buttonContainer.height() / numKids);
     var heightCount = 0;
     var lastButtonHeight = 0;
     for (var i = 0; i < numKids-1; i++) {
-        var toChange = document.getElementById('buttonDiv' + i);
-        toChange.style.height = newHeight + 'px';
+        var $toChange = $('#buttonDiv' + i);
+        $toChange.height(newHeight + 'px');
         heightCount += newHeight;
         console.log(heightCount);
     }
-    lastButtonHeight = buttonContainer.clientHeight - heightCount;
-    buttonContainer.lastChild.style.height = lastButtonHeight;
+    lastButtonHeight = ($buttonContainer.height() - heightCount) - numKids;
+    $buttonContainer.children().last().height(lastButtonHeight + 'px');
 }
 
 function clickedFunc(buttonClicked) {
